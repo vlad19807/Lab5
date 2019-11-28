f3c84df36075915228e028aeff0f3e53f13528fa
vlad19807 <55804615+vlad19807@users.noreply.github.com>
2019-11-28 12:17:05 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,47 @@
+# include <stdio.h>
+int main()
+{    
+    int pid;
+    pid=getpid();
+    
+    printf("Current Process ID is : %d\n",pid);
+
+    printf("[ Forking Child Process ... ] \n");    
+    pid=fork(); /* This will Create Child Process and
+               Returns Child's PID */
+if(pid < 0)
+    {
+        /* Process Creation Failed ... */
+
+
+        exit(-1);
+    }
+    elseif(pid==0) 
+    {
+        /* Child Process */
+
+
+        printf("Child Process Started ...\n");
+        printf("Child Process Completed ...\n");
+        printf("I am In Zombie State ...\n");
+    }
+    else 
+    {
+        /* Parent Process */
+
+
+        sleep(10);
+        printf("Parent Process Running ... \n");
+        
+        while(1)
+        {
+            /* 
+                Infinite Loop that Keeps the
+                   Process Running
+            */
+
+
+        }
+    }    
+    return 0;
+}
