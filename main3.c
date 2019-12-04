3af1d50aaf390e275be86b4f1e453a726da9bf33
vlad19807 <55804615+vlad19807@users.noreply.github.com>
2019-12-04 20:47:07 +0200

    Update main3.c

--- main3.c
+++ main3.c
@@ -1,6 +1,9 @@
-#include <sys/types.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> 
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 int main()
 {
 char pid{[}255{]};
